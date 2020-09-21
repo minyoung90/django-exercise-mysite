06ac1011e6e47653405547e347e1b45b0f1e3be8
minyoung90 <asakura14@naver.com>
2020-09-21 10:57:22 +0900

    feat: auth

--- blog/urls.py
+++ blog/urls.py
@@ -33,4 +33,8 @@ urlpatterns = [
     path("tag/<str:tag>/", views.TaggedObjectLV.as_view(), name="tagged_object_list"),
     # Example: /blog/search/
     path("search/", views.SearchFormView.as_view(), name="search"),
+    path("add/", views.PostCreateView.as_view(), name="add"),
+    path("change/", views.PostChangeLV.as_view(), name="change"),
+    path("<int:pk>/update/", views.PostUpdateView.as_view(), name="update"),
+    path("<int:pk>/delete/", views.PostDeleteView.as_view(), name="delete"),
 ]
